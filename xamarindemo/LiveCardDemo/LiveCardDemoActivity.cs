1d043ac6d45d411e69c04fc3933330a57346e778
Jon Carlos <jon@screenmedia.co.uk>
2014-01-09 16:54:29 +0000

    Converted most of the classes to C#, this has not been tested on Glass yet so it's unlikley to work

--- xamarindemo/LiveCardDemo/LiveCardDemoActivity.cs
+++ xamarindemo/LiveCardDemo/LiveCardDemoActivity.cs
@@ -5,19 +5,145 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Util;
+using Android.Glass.App;
+using Android.Glass.Widget;
+using Android.Glass.Touchpad;
 
 namespace LiveCardDemo
 {
 	[Activity (Label = "LiveCardDemo", MainLauncher = true)]
-	public class MainActivity : Activity
+	public class LiveCardDemoActivity : Activity//, Android.Views.GestureDetector.IOnGestureListener, Android.Glass.Touchpad.GestureDetector.IFingerListener
 	{
+		// Service to handle liveCard publishing, etc...
+		private bool mIsBound = false;
+		private static LiveCardDemoLocalService liveCardDemoLocalService;
+		private static string _tag = "LiveCardDemo.LiveCardDemoActivity";
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
-			SetContentView (Resource.Layout.Activity_LiveCardDemo);
+			Log.Debug(_tag, "onCreate() called.");
+
+			SetContentView(Resource.Layout.Activity_LiveCardDemo);
+
+			// Service to control the life cycle of a LiveCard.
+			// bind() does not work. We need to call start() explicitly...
+			// doBindService();
+			DoStartService();
+			// TBD: We need to call doStopService() when the user "closes" the app....
+			// ...
+		}
+
+		protected override void OnDestroy()
+		{
+			DoUnbindService();
+			base.OnDestroy();
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			Log.Debug(_tag, "onResume() called.");
+
+			// For live card menu handling
+			OpenOptionsMenu();
+		}
+
+
+		// Context menus
+		// ...
+
+		public override bool OnCreateOptionsMenu (IMenu menu)
+		{
+			Log.Debug(_tag, "onCreateOptionsMenu() called.");
+
+			MenuInflater inflater = this.MenuInflater;
+			inflater.Inflate(Resource.Layout.Livecard_LiveCardDemo, menu);
+			return true;
+		}
+
+		public override bool OnOptionsItemSelected(IMenuItem item)
+		{
+			Log.Debug(_tag, "onOptionsItemSelected() called.");
+
+			// Handle item selection.
+			switch (item.ItemId) {
+			case Resource.Id.menu_stop_livecarddemo:
+				DoStopService();
+				return true;
+			default:
+				return base.OnOptionsItemSelected(item);
+			}
+		}
+		
+		public override void OnOptionsMenuClosed(IMenu menu)
+		{
+			Log.Debug(_tag, "onOptionsItemSelected() called.");
+
+			// Nothing else to do, closing the activity.
+			Finish();
+		}
+
+//		private ServiceConnection serviceConnection = new ServiceConnection() {
+//			public void OnServiceConnected(ComponentName className, IBinder service) {
+//				Log.d("onServiceConnected() called.");
+//				liveCardDemoLocalService = ((LiveCardDemoLocalService.LocalBinder)service).getService();
+//			}
+//			public void OnServiceDisconnected(ComponentName className) {
+//				Log.d("onServiceDisconnected() called.");
+//				liveCardDemoLocalService = null;
+//			}
+//		};
+
+		public class MyServiceConnection : IServiceConnection {
+			#region IServiceConnection implementation
+			public void OnServiceConnected(ComponentName className, IBinder service) {
+				Log.Debug(_tag, "onServiceConnected() called.");
+				liveCardDemoLocalService = ((LiveCardDemoLocalService.LocalBinder)service).GetService();
+			}
+			public void OnServiceDisconnected(ComponentName className) {
+				Log.Debug(_tag, "onServiceDisconnected() called.");
+				liveCardDemoLocalService = null;
+			}
+			#endregion
+			#region IDisposable implementation
+			public void Dispose ()
+			{
+				//throw new NotImplementedException ();
+			}
+			#endregion
+			#region IJavaObject implementation
+			public IntPtr Handle {
+				get {
+					return new IntPtr ();
+				}
+			}
+			#endregion
+
+		}
+
+		private IServiceConnection serviceConnection = new MyServiceConnection();
+
+		private void DoBindService()
+		{
+			BindService(new Intent(this, typeof(LiveCardDemoLocalService)), serviceConnection, Bind.AutoCreate);
+			mIsBound = true;
+		}
+		private void DoUnbindService() {
+			if (mIsBound) {
+				UnbindService(serviceConnection);
+				mIsBound = false;
+			}
+		}
+		private void DoStartService()
+		{
+			StartService(new Intent(this, typeof(LiveCardDemoLocalService)));
+		}
+		private void DoStopService()
+		{
+			StopService(new Intent(this, typeof(LiveCardDemoLocalService)));
 		}
 	}
 }
-
-
---
+++ xamarindemo/LiveCardDemo/Service/LiveCardDemoLocalService.cs
@@ -0,0 +1,154 @@
+using System;
+using Android.App;
+using Android.Glass.Timeline;
+using Android.OS;
+using Android.Util;
+using Android.Content;
+using Android.Widget;
+
+namespace LiveCardDemo
+{
+	public class LiveCardDemoLocalService : Service
+	{
+		public LiveCardDemoLocalService () : base()
+		{
+		}
+
+		private string _tag = "LiveCardDemo.Service.LiveCardDemoLocalService";
+
+		// "Life cycle" constants
+
+		// [1] Starts from this..
+		private static int STATE_NORMAL = 1;
+
+		// [2] When panic action has been triggered by the user.
+		private static int STATE_PANIC_TRIGGERED = 2;
+
+		// [3] Note that cancel, or successful send, etc. change the state back to normal
+		// These are intermediate states...
+		private static int STATE_CANCEL_REQUESTED = 4;
+		private static int STATE_CANCEL_PROCESSED = 8;
+		private static int STATE_PANIC_PROCESSED = 16;
+		// ....
+
+		// Global "state" of the service.
+		// Currently not being used...
+		private int currentState;
+
+
+		// For live card
+		private LiveCard liveCard;
+		static LiveCardDemoLocalService _liveCardDemoLocalService = new LiveCardDemoLocalService();
+
+		// No need for IPC...
+		public class LocalBinder : Binder {
+			public LiveCardDemoLocalService GetService() {
+				return _liveCardDemoLocalService;
+			}
+		}
+		private IBinder mBinder = new LocalBinder();
+
+
+		public override void OnCreate()
+		{
+			base.OnCreate();
+
+			currentState = STATE_NORMAL;
+		}
+
+		public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+		{
+			Log.Info(_tag, "Received start id " + startId + ": " + intent);
+			OnServiceStart();
+			return StartCommandResult.Sticky;
+		}
+
+		public override IBinder OnBind(Intent intent)
+		{
+			// ????
+			OnServiceStart();
+			return mBinder;
+		}
+
+		public override void OnDestroy()
+		{
+			// ???
+			OnServiceStop();
+
+			base.OnDestroy();
+		}
+
+
+		// Service state handlers.
+		// ....
+
+		private bool OnServiceStart()
+		{
+			Log.Debug(_tag, "onServiceStart() called.");
+
+			// TBD:
+			// Publish live card...
+			// ....
+			publishCard(this);
+			// ....
+
+			currentState = STATE_NORMAL;
+			return true;
+		}
+
+		private bool OnServicePause()
+		{
+			Log.Debug(_tag, "onServicePause() called.");
+			return true;
+		}
+		private bool OnServiceResume()
+		{
+			Log.Debug(_tag, "onServiceResume() called.");
+			return true;
+		}
+
+		private bool OnServiceStop()
+		{
+			Log.Debug(_tag, "onServiceStop() called.");
+
+			// TBD:
+			// Unpublish livecard here
+			// .....
+			unpublishCard(this);
+			// ...
+
+			return true;
+		}
+
+
+		// For live cards...
+
+		private void publishCard(Context context)
+		{
+			Log.Debug(_tag, "publishCard() called.");
+			if (liveCard == null) {
+				String cardId = "livecarddemo_card";
+				TimelineManager tm = TimelineManager.From(context);
+				liveCard = tm.CreateLiveCard(cardId);
+
+				liveCard.SetViews(new RemoteViews(context.PackageName, Resource.Layout.Livecard_LiveCardDemo));
+				Intent intent = new Intent(context, typeof(LiveCardDemoActivity));
+				liveCard.SetAction(PendingIntent.GetActivity(context, 0, intent, 0));
+				liveCard.Publish(LiveCard.PublishMode.Silent);
+			} else {
+				// Card is already published.
+				return;
+			}
+		}
+
+		private void unpublishCard(Context context)
+		{
+			Log.Debug(_tag, "unpublishCard() called.");
+			if (liveCard != null) {
+				liveCard.Unpublish();
+				liveCard = null;
+			}
+		}
+	}
+}
+
